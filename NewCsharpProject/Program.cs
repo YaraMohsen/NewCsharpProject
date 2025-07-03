91e49ef2784414b9da8edaccfa027660fbd89dac
YaraMohsen <yaramohsen549@gmail.com>
2025-07-03 15:18:36 +0300

    question5

--- NewCsharpProject/Program.cs
+++ NewCsharpProject/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+class test_ref
+{
+    public int x = 10;
+    public int y = 20;
+}
 
 namespace NewCsharpProject
 {
@@ -27,10 +32,36 @@ namespace NewCsharpProject
 
             #region Question3
 
-            float DividToInt = 10.6f;
-            int int_value = (int)DividToInt; //explicit casting
+            //float DividToInt = 10.6f;
+            //int int_value = (int)DividToInt; //explicit casting
 
-            Console.WriteLine(int_value);
+            //Console.WriteLine(int_value);
+            #endregion
+
+
+            #region Question4
+
+            #endregion
+
+            #region Question5
+            //int y = 5;
+            //int x = y;
+            //x++;
+            //Console.WriteLine($"{x},{y}");
+            #endregion
+
+            #region Question6
+            test_ref new_obj1 = new test_ref();
+            test_ref new_obj2 = new test_ref();
+
+            new_obj1.x = 7;
+            new_obj2 = new_obj1;
+
+            new_obj2.x = 9;
+
+            Console.WriteLine($"new_obj1.x = {new_obj1.x}, new_obj2.x = {new_obj2.x}"); 
+            // new_obj1 .x will also be 9 because new_obj2 is a reference to the same object as new_obj1
+
             #endregion
         }
     }

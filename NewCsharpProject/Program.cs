81d0b527ee679553b14d4eb75fbf8e4b426a6fd2
YaraMohsen <yaramohsen549@gmail.com>
2025-07-03 15:12:55 +0300

    Question2

--- NewCsharpProject/Program.cs
+++ NewCsharpProject/Program.cs
@@ -8,10 +8,20 @@ namespace NewCsharpProject
         {
             #region Question1
 
-            Console.WriteLine("enter the number");
-            String read_number = Console.ReadLine();
+            //Console.WriteLine("enter the number");
+            //String read_number = Console.ReadLine();
 
-            Console.WriteLine("You enter :" + read_number);
+            //Console.WriteLine("You enter :" + read_number);
+
+            #endregion
+
+            #region Question2
+
+            String IntStringVar = "150A";
+
+            int.TryParse(IntStringVar, out int res);
+            Console.WriteLine("The string is a valid integer: " + res);
+
 
             #endregion
         }

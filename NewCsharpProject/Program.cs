e6da4f9b86df9c63d7c51659e95a3f702b8c0c0c
YaraMohsen <yaramohsen549@gmail.com>
2025-07-03 17:33:22 +0300

    question8

--- NewCsharpProject/Program.cs
+++ NewCsharpProject/Program.cs
@@ -266,16 +266,30 @@ namespace NewCsharpProject
 
             #region Write a program that allows the user to insert an integer then print a multiplication table up to 12
 
-            Console.Write("Enter integer: ");
-            int num = Convert.ToInt32(Console.ReadLine());
-            for (int i = 1; i <= 12; i++)
-            {
-                Console.WriteLine($"{i}*{num}= {i * num}");
-            }
+            //Console.Write("Enter integer: ");
+            //int num = Convert.ToInt32(Console.ReadLine());
+            //for (int i = 1; i <= 12; i++)
+            //{
+            //    Console.WriteLine($"{i}*{num}= {i * num}");
+            //}
 
             #endregion
 
+            #region Write a program that allows to user to insert number then print all even numbers between 1 to this number
 
+            Console.Write("Enter integer: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= num; i++)
+            {
+                if(i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+                
+            }
+
+            #endregion
 
             #endregion
 
